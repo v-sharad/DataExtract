cc3e5f87e58884c8202518980dccd0f2cff493e6
v.sharad <v.sharad@outlook.com>
2017-10-12 23:24:21 +0530

    Get the consolidated data from the value research website

--- value_research_data_import.py
+++ value_research_data_import.py
@@ -1,17 +1,23 @@
 import pandas as pd
+from functools import reduce 
 from os import listdir
 
 def import_mutual_funds_data(path):
     files = listdir(path)
     mutual_funds_data = {}
     for file in files:
-        file_path = path + "\\" + file
-        file_data = pd.read_csv(file_path,skiprows=[0,1])
-        file_contents = file.split('-')[1]
-        mutual_funds_data[file_contents] = file_data
+        if file.find("ValueResearch") > -1:
+            file_path = path + "\\" + file
+            file_data = pd.read_csv(file_path,skiprows=[0,1])
+            file_contents = file.split('-')[1]
+            mutual_funds_data[file_contents] = file_data
     return mutual_funds_data
 
-path = r"C:\Users\venkats4\Documents\Investments\Multi Cap"
+path = r"C:\Users\Meow\Documents\Investments\Multi Cap"
 funds_data = import_mutual_funds_data(path)
-
-test = reduce(lambda left,right:pd.merge(left,right,on="Fund"),funds_data.values())
+nav = funds_data.pop('NavDetails') #NavDetails Dataframe has to be cleaned
+nav = nav[nav['Scheme'].str.endswith('-G') ==  True] #Select only the Growth option schemes
+nav = nav.reset_index(drop = True) #reset the indexes completely to allow accurate merge 
+consolidated_funds_data = reduce(lambda left,right:pd.merge(left,right,on=["Fund","Rating"]),funds_data.values())
+consolidated_funds_data = consolidated_funds_data.merge(nav,how = "outer",left_index =True,right_index = True)
+consolidated_funds_data.to_csv(path + "\\" + "consolidated.csv")
